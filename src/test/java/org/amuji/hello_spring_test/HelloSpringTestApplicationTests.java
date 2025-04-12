dd1f66d8e2121f92721ed7d603154746d2e81a17
royyang <aroundall@gmail.com>
2025-04-12 10:31:52 +0800

    init

---
+++ src/test/java/org/amuji/hello_spring_test/HelloSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amuji.hello_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
